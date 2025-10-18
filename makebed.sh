e85c7c6f0df8d05724e13172602218b1fa14c006
nanditajgupta <146375925+nanditajgupta@users.noreply.github.com>
2025-10-18 14:36:47 -0700

    Add files via upload

---
+++ makebed.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#SBATCH --job-name=bam_to_bed
+#SBATCH --account=minjilab0
+#SBATCH --output=/nfs/turbo/umms-minjilab/njgupta/chromsig/makebed_output/slurm-%j.out
+
+function usage
+{
+    echo -e "DESCRIPTION:
+    This script only runs if a .bam file was input to the main program. It uses bedtools to convert from .bam to .bed.
+    USAGE:
+    sbatch makebed.sh --bp bam_prefix --bam bam_file --dir directory --str str_type
+    
+        ARGUMENTS:
+            --bp    Name of the bam file with .bam extension removed (ex: For GM12878_ChIP-seq_RAD51_ENCFF725MWO.bam this would be GM12878_ChIP-seq_RAD51_ENCFF725MWO).
+            --bam   Name of the bam file.
+            --dir   Directory in which the bam file is stored.
+            --str   The type of read, Single or Paired End (ex: SE or PE).
+        
+        EXAMPLE:
+            sbatch makebed.sh --bp GM12878_ChIP-seq_RAD51_ENCFF725MWO --bam GM12878_ChIP-seq_RAD51_ENCFF725MWO.bam --dir /Users/chromsig/Data/bam_file_name/ --str PE"
+}
+
+## Parse arguments from the command line
+while [ "$1" != "" ]; do
+    case $1 in
+        -s | --bp )          shift
+                                bam_prefix=$1
+                                ;;
+        -s | --bam )         shift
+                                bam_file=$1
+                                ;;
+        -s | --dir )         shift
+                                directory=$1
+                                ;;
+        -s | --str )         shift
+                                str_type=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+cd ${directory}
+
+# using bedtools, convert bam input to bed file (this runs only if the input was a bam file)
+# For Single End, do a direct conversion.
+# For Paired End, eliminate any unpaired reads, and only convert paired reads to bed file format.
+
+echo "Bed file being made: ${bam_prefix}_bam2bed.bed";
+if [ $str_type == "SE" ]
+then
+    bedtools bamtobed -i ${bam_file} > "${bam_prefix}_bam2bed.bed";
+else
+    pwd
+    echo "samtools view -bf 0x2 ${bam_file} | bedtools bamtobed -i stdin > "${bam_prefix}_bam2bed.bed"";
+    samtools view -bf 0x2 ${bam_file} | bedtools bamtobed -i stdin > "${bam_prefix}_bam2bed.bed";
+fi
+
+echo "Bed file made"
---
+++ makestdbed.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH --job-name=bed_to_stdbed
+#SBATCH --account=minjilab0
+#SBATCH --time=00-24:00:00
+#SBATCH --output=/nfs/turbo/umms-minjilab/njgupta/chromsig/makestdbed_output/slurm-%j.out
+
+function usage
+{
+    echo -e "DESCRIPTION:
+    This script sorts the .bed file, and then runs it through the python program Read_Bed.py to remove any non-standard chromosomes.
+    USAGE:
+    sbatch makestdbed.sh --bp bam_prefix --bed bed_file --dir directory --ref ref
+    
+        ARGUMENTS:
+            --bp    Name of the bam file with .bam extension removed (ex: For GM12878_ChIP-seq_RAD51_ENCFF725MWO.bam this would be GM12878_ChIP-seq_RAD51_ENCFF725MWO).
+            --bed   Name of the bed file.
+            --dir   Directory in which the bam file is stored.
+            --ref   Name of the reference genome (ex: hg38).
+        
+        EXAMPLE:
+            sbatch makestdbed.sh --bp GM12878_ChIP-seq_RAD51_ENCFF725MWO --bam GM12878_ChIP-seq_RAD51_ENCFF725MWO_bam2bed.bed --dir /Users/chromsig/Data/bam_file_name/ --ref hg38"
+}
+
+## Parse arguments from the command line
+while [ "$1" != "" ]; do
+    case $1 in
+        -s | --bp )          shift
+                                bam_prefix=$1
+                                ;;
+        -s | --bed )         shift
+                                bed_file=$1
+                                ;;
+        -s | --dir )         shift
+                                directory=$1
+                                ;;
+        -s | --ref )         shift
+                                ref=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+# Sort the bed file by chromosome, and then by index (some files such as CUT&RUN may be out of order)
+
+echo "Sorting Bed"
+sort -k 1,1 -k2,2n ${directory}${bam_prefix}_bam2bed.bed > ${directory}${bam_prefix}_sorted_bam2bed.bed
+echo "Bed Sorted"
+sorted_bed="${bam_prefix}_sorted_bam2bed.bed"
+
+# Eliminate any non-standard chromsomes using the reference genome file to form a standard bed file.
+
+echo "Standard Bed file being made: ${bam_prefix}_bam2bed.std.bed";
+python Read_Bed.py "${directory} ${sorted_bed} ${ref}.chrom.sizes";
+echo "Standard Bed file made"
